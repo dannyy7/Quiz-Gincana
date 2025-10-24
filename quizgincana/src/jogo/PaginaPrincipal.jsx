60ea8f91787f7bd36666530a8b7893128f457b91
dannyy7 <163425282+dannyy7@users.noreply.github.com>
2025-10-24 17:10:38 -0300

    alterações em imagens
    Provavelmente será necessario arrumar a "hitbox" dos elementos da tela

--- quizgincana/src/jogo/PaginaPrincipal.jsx
+++ quizgincana/src/jogo/PaginaPrincipal.jsx
@@ -8,13 +8,14 @@ function PaginaPrincipal(){
         <div className={styles.container}>
             <div className="coluna">
                 <div className={styles.bloco1}></div>
-                <img src='pagina_principal/pgprincipalmoldura.png' id='moldura'></img>
-                <img src='pagina_principal/pgprincipalsetadireita.png'></img>
-                <img src='pagina_principal/pgprincipalsetaesquerda.png'></img>
-                <img src='pagina_principal/pgprincipalbutton.png'></img>
+                <img src='pagina_principal/pgprincipalmoldura.png' className={styles.imagem}></img>
+                <img src='pagina_principal/pgprincipalsetadireita.png' className={styles.imagem}></img>
+                <img src='pagina_principal/pgprincipalsetaesquerda.png' className={styles.imagem}></img>
+                <img src='pagina_principal/pgprincipalbutton.png'className={styles.imagem}></img>
+                <button className={styles.botao}></button>
              </div>
 
-            <img src='pagina_principal/pgprincipalbutton2.png'></img>
+            <img src='pagina_principal/pgprincipalbutton2.png' className={styles.imagem}></img>
         </div>
     )
 }
