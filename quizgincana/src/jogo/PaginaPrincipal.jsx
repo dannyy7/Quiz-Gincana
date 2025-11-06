153dfb1c69328c019c677806826466c1cfc53131
eve2005 <163425282+dannyy7@users.noreply.github.com>
2025-11-06 17:01:48 -0300

    CSS página principal
    apenas testes do css da página principal. Ainda precisamos pensar em como faremos a responsividade do site e como ele irá ficar na versão para celular

--- quizgincana/src/jogo/PaginaPrincipal.jsx
+++ quizgincana/src/jogo/PaginaPrincipal.jsx
@@ -9,9 +9,14 @@ function PaginaPrincipal() {
     <div className={styles.container}>
 
       <img src="/pagina_principal/pgprincipalsombra.png" className={styles.imagem}/>{/* efeitos(Imagens da sombra e luz)*/}
+      <div className={styles.caixanome}>
+        
+        <p className={styles.nome}>convidado</p>
       
+      </div>
+
       <div className={styles.coluna}>
-        <div className={styles.linha}>
+        <div  className={`${styles.linha} ${styles.centro}`}>
           <button className={styles.botao}>
           <img src="/pagina_principal/pgprincipalsetaesquerda.png" />
           </button>
@@ -25,20 +30,28 @@ function PaginaPrincipal() {
             <img src="/pagina_principal/pgprincipalsetadireita.png" />
           </button>
         </div>
-        <input></input>
-        <button
-          className={styles.botao}
-          onMouseEnter={() =>
-            setBotao('/pagina_principal/pgprincipaljogar2.png')
-          }
-          onMouseLeave={() =>
-            setBotao('/pagina_principal/pgprincipaljogar1.png')
-          }
-        >
-          <img src={botao} />
-        </button>
+        <div className={`${styles.coluna} ${styles.centro2}`}>
+          <input className={styles.codigo} type='number'></input>
+          <button
+            className={`${styles.botao} ${styles.jogar}`}
+            onMouseEnter={() =>
+              setBotao('/pagina_principal/pgprincipaljogar2.png')
+            }
+            onMouseLeave={() =>
+              setBotao('/pagina_principal/pgprincipaljogar1.png')
+            }
+          >
+            <img src={botao} />
+          </button>
+        </div>
+
       </div>
-      <button className={styles.botao}><img src="/pagina_principal/pgprincipallogar.png"/></button>
+      <div className={styles.quizes}>
+          <button className={styles.botao}><img src="/pagina_principal/pgprincipallogar.png"/></button>
+          <div className={styles.boxli}>
+            <li></li>{/* essa será a lista de objetos(quizes) criados pelo usuario */}
+          </div>
+      </div>
 
     </div>
   );
