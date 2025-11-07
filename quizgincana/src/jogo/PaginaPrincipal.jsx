63beb5d7a15699d3313a2f6cf03416b1563c3411
dannyy7 <danielarodlercordeiro@gmail.com>
2025-11-07 11:57:04 -0300

    CSS refeito
    Houve um mudança na edição do css agora boa parte foi feito com posição absoluta e uma pequena parte com flex-box. O código foi refeito do zero.

--- quizgincana/src/jogo/PaginaPrincipal.jsx
+++ quizgincana/src/jogo/PaginaPrincipal.jsx
@@ -7,52 +7,40 @@ function PaginaPrincipal() {
 
   return (
     <div className={styles.container}>
-
-      <img src="/pagina_principal/pgprincipalsombra.png" className={styles.imagem}/>{/* efeitos(Imagens da sombra e luz)*/}
-      <div className={styles.caixanome}>
-        
-        <p className={styles.nome}>convidado</p>
-      
-      </div>
-
-      <div className={styles.coluna}>
-        <div  className={`${styles.linha} ${styles.centro}`}>
-          <button className={styles.botao}>
-          <img src="/pagina_principal/pgprincipalsetaesquerda.png" />
-          </button>
-
-          <img
-            src="/pagina_principal/pgprincipalmoldura.png"
-            className={styles.tamanho}
-          />
-
-          <button className={styles.botao}>
-            <img src="/pagina_principal/pgprincipalsetadireita.png" />
-          </button>
-        </div>
-        <div className={`${styles.coluna} ${styles.centro2}`}>
-          <input className={styles.codigo} type='number'></input>
-          <button
-            className={`${styles.botao} ${styles.jogar}`}
-            onMouseEnter={() =>
+  
+        <div className={styles.fundo2}></div>
+        <img src='/pagina_principal/pgprincipalmoldura.png' className={styles.moldura}></img>
+        <button className={styles.setadireita}><img src='/pagina_principal/pgprincipalsetadireita.png'></img></button>
+        <button className={styles.setaesquerda}><img src='/pagina_principal/pgprincipalsetaesquerda.png'></img></button>
+        <div className={styles.nomebox}><p className={styles.nome}>convidado</p></div>
+        <input className={styles.codigobox}></input>
+        <button onMouseEnter={() =>
               setBotao('/pagina_principal/pgprincipaljogar2.png')
             }
             onMouseLeave={() =>
               setBotao('/pagina_principal/pgprincipaljogar1.png')
-            }
-          >
-            <img src={botao} />
-          </button>
+            } className={styles.jogar}><img src={botao}/></button>
+        <img src='/pagina_principal/pgprincipallogar.png' className={styles.logar}></img>
+
+        <div className={styles.listabox}>
+
+          <div>
+            <ul>
+              <button>+</button>
+              <li>quiz1</li>
+              <li>quiz2</li>
+              <li>quiz3</li>
+            </ul>
+        
+          </div>
+
+          <div>{/* Falso Placeholder nas listas de quizes (usando opacity no css*/ }
+            <p></p>
+          </div>
+
         </div>
 
-      </div>
-      <div className={styles.quizes}>
-          <button className={styles.botao}><img src="/pagina_principal/pgprincipallogar.png"/></button>
-          <div className={styles.boxli}>
-            <li></li>{/* essa será a lista de objetos(quizes) criados pelo usuario */}
-          </div>
-      </div>
-
+      
     </div>
   );
 }
